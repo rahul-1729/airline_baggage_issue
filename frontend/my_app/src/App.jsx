1c3a655b4a5a4c34239e5ae2e6874b881eb856b6
Rahul Kumar (Business Guest) <b-rahulkumar@microsoft.com>
2024-12-27 12:15:27 +0530

    Login done

--- frontend/my_app/src/App.jsx
+++ frontend/my_app/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Signup from "./components/Signup"
+import Login from "./components/Login"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       <Signup/>
+       {/* <Signup/> */}
+       <Login/>
     </>
   )
 }
